f5908b01c238239097cb03390c20a10ebc2ea9b6
Troy E Martin <troydotmartin@gmail.com>
2016-09-21 10:44:20 -0500

    Removed 'favorited' property.  Changed search terms.  Added a place to put a condition in if we only want to save tweets with certain properties, to test tracking certain info.

--- MiningListener.py
+++ MiningListener.py
@@ -9,19 +9,24 @@ class MineListener(tweepy.StreamListener):
     def on_status(self, status):
 
         print "Got a tweet..."
-        tweet_id = status.id
-        text = status.text
-        retweet_count = status.retweet_count
-        quotes_count = "--not implemented--"  # HOW to get this?
-        favorited = status.favorited
-        favorites_count = status.favorite_count  # Whether or not tweet was favorited rather than number of favorites
-        author = status.author
-        is_news = "--not implemented--"  # DEFINE LIST OF VALID NEWS AUTHORS
 
-        tweet = Tweet.Tweet(tweet_id=tweet_id, text=text, retweet_count=retweet_count,
-                            quotes_count=quotes_count, favorited=favorited,favorites_count=favorites_count,
-                            author=author, is_news=is_news)
-        MineListener.tweets.append(tweet)
+        if 1: #Add a condition for saving a tweet // Contains emoji, favorites, etc to test 
+            print "Accepted!"
+            tweet_id = status.id
+            text = status.text
+            retweet_count = status.retweet_count
+            quotes_count = "--not implemented--"  # HOW to get this?
+            favorites_count = status.favorite_count
+            author = status.author
+            is_news = "--not implemented--"  # DEFINE LIST OF VALID NEWS AUTHORS
+
+            tweet = Tweet.Tweet(tweet_id=tweet_id, text=text, retweet_count=retweet_count,
+                                quotes_count=quotes_count, favorites_count=favorites_count,
+                                author=author, is_news=is_news)
+            MineListener.tweets.append(tweet)
+        else:
+            print "Rejected..."
+
 
         if len(MineListener.tweets) >= 10:
             return False
--- Tweet.py
+++ Tweet.py
@@ -28,20 +28,19 @@ class Tweet:
 
 
 #Derive: SENTIMENT, CAPITALS, QUESTION MARKS, EMOTICONS
-	def __init__(self, tweet_id, text, author, retweet_count, quotes_count, favorited, favorites_count, is_news):
+	def __init__(self, tweet_id, text, author, retweet_count, quotes_count, favorites_count, is_news):
 		self.tweet_id = tweet_id
 		self.text = text
 		self.author = author
 		self.retweet_count = retweet_count
 		self.quotes_count = quotes_count
-		self.favorited = favorited
 		self.favorites_count = favorites_count
 		self.is_news = is_news
 		
 		afinn = Afinn()
+		self.sentiment = afinn.score(text)
 		self.verified = self.author.verified
 		self.follower_count = self.author.followers_count
-		self.sentiment = afinn.score(text)
 		self.capitals = self.countCapitals(text)
 		self.exclamationmarks = self.countExclamationMarks(text)
 		self.questionmarks = self.countQuestionMarks(text)
@@ -57,8 +56,7 @@ class Tweet:
 		print 'verified: '+str(self.verified)
 		print 'retweet_count: '+str(self.retweet_count)
 		print 'quotes_count: '+str(self.quotes_count)
-		print 'favorites_count: '+str(self.favorited)
-		print 'favorited: '+str(self.favorites_count)
+		print 'favorites_count: '+str(self.favorites_count)
 		print 'followers_count: '+str(self.follower_count)
 		print 'capitals: '+str(self.capitals)
 		print 'questionmarks: '+str(self.questionmarks)
