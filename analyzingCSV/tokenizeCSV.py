ecc8d4cc0ebbe19a9ee0c1e4a3068cfb26064f33
Benjamin <bjmlmorrison@gmail.com>
2016-10-03 02:26:06 -0700

    some minor changes with no need to look

--- analyzingCSV/tokenizeCSV.py
+++ analyzingCSV/tokenizeCSV.py
@@ -1,7 +1,21 @@
 import nltk, csv
 from nltk.tokenize import sent_tokenize, word_tokenize 
 
-path = '2016-10-03 08:07:52.csv'
+path = 'Twitter file'
+#read the file and tokenize and tagging the data into POS_TAG:
+#Tag 	Meaning 	English Examples
+#ADJ 	adjective 	new, good, high, special, big, local
+#ADP 	adposition 	on, of, at, with, by, into, under
+#ADV 	adverb 	really, already, still, early, now
+#CONJ 	conjunction 	and, or, but, if, while, although
+#DET 	determiner, article 	the, a, some, most, every, no, which
+#NOUN 	noun 	year, home, costs, time, Africa
+#NUM 	numeral 	twenty-four, fourth, 1991, 14:24
+#PRT 	particle 	at, on, out, over per, that, up, with
+#PRON 	pronoun 	he, their, her, its, my, I, us
+#VERB 	verb 	is, say, told, given, playing, would
+#. 	punctuation marks 	. , ; !
+#X 	other 	ersatz, esprit, dunno, gr8, univeristy
 with open(path, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
