72afa691dd53b464c71ce8e831a18adf359a7a14
Archit Gupta <archit.941@gmail.com>
2016-10-23 14:09:23 -0500

    Regression Plot Avg Stock vs Sentiment per day

---
+++ Regression_Plot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('date_to_ss',header=0)
+sentiment = df['Sentiment_Score']
+stock_val=df['Stock_Value'] #Change According to Column name in date_to_ss
+
+ax_sent=sns.regplot(x=sentiment,y=stock_val,data=df)
+ax_sent.set_title("Stock Value v Sentiment Score")
+
+plt.show()
